f3aef00038b06d1f5c22d8ee14ba31637bb0b08b
gmachado <gmachado@student.42sp.org.br>
2022-04-12 22:19:54 -0300

    Completed ft_lst*() functions.

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 15:27:02 by gmachado          #+#    #+#             */
+/*   Updated: 2022/04/12 16:36:17 by gmachado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	while ((*lst)->next != NULL)
+		*lst = (*lst)->next;
+	(*lst)->next = new;
+}
