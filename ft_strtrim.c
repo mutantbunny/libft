8c70182c2e9afc81b99be9e9b12b44c1edea2dc9
gmachado <gmachado@student.42sp.org.br>
2022-05-12 03:13:42 -0300

    Fixed bug in ft_strtrim: array size should be 256, not 255.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 20:51:32 by gmachado          #+#    #+#             */
-/*   Updated: 2022/04/13 22:24:16 by gmachado         ###   ########.fr       */
+/*   Updated: 2022/04/23 23:46:16 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	charmap[255];
+	char	charmap[256];
 	size_t	start;
 	size_t	end;
 
@@ -24,7 +24,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(""));
 	if (set == NULL)
 		return (ft_strdup(s1));
-	ft_bzero(charmap, 255);
+	ft_bzero(charmap, 256);
 	while (*set != '\0')
 	{
 		charmap[(int)(*set)] = 1;
